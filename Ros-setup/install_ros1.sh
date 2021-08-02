40ccb993392337f4a898bb68a29a60a696102b79
eliastreis <82460419+eliastreis@users.noreply.github.com>
2021-08-02 18:06:03 +0200

    Update install_ros1.sh

--- Ros-setup/install_ros1.sh
+++ Ros-setup/install_ros1.sh
@@ -43,3 +43,28 @@ echo "export WORKON_HOME=$HOME/python_env   #path to your venv folder
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3   #path to your python3 
 export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
 source /usr/local/bin/virtualenvwrapper.sh" >> ~/.zshrc
+source $HOME/.zshrc
+mkvirtualenv --python=python3.6 rosnav
+workon rosnav
+pip install --extra-index-url https://rospypi.github.io/simple/ rospy rosbag tf tf2_ros --ignore-installed
+pip install pyyaml catkin_pkg netifaces pathlib
+pip install stable-baselines3
+
+#arena-rosnav related downloads
+cd $HOME
+mkdir -p catkin_ws/src && cd catkin_ws/src
+git clone https://github.com/ignc-research/arena-rosnav
+cd arena-rosnav && rosws update
+source $HOME/.zshrc
+cd $HOME/catkin_ws/src/
+git clone https://github.com/eliastreis/arena-rosnav-3D.git
+cd ../.. 
+catkin_make -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3
+source devel/setup.zsh
+cd $HOME/catkin_ws/src/arena-rosnav
+./geometry2_install.sh
+cd $HOME/geometry2_ws
+catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3
+
+
+
