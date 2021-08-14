41ce2c6b01b6feb47504cd5e896cb96228d42545
eliastreis <elias.treis@gmail.com>
2021-08-14 20:04:07 +0200

    incl. maps

---
+++ src/build/aws_robomaker_hospital_world/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/elias/catkin_ws/src/arena-rosnav-3D/src/build/aws_robomaker_hospital_world/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/elias/.local/bin:/home/elias/.local/bin:/home/elias/.local/bin:/home/elias/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export ROS_PACKAGE_PATH="/home/elias/catkin_ws/src/arena-rosnav-3D/src/aws-robomaker-hospital-world:$ROS_PACKAGE_PATH"
