f1be62347fd7324680754ff69dd5ecefdc078db5
Jacenty00 <j.kmiecik@campus.tu-berlin.de>
2021-09-22 18:18:35 +0200

    Formatting

--- task_generator/scripts/task_generator_node.py
+++ task_generator/scripts/task_generator_node.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
-import rospy, time
+import rospy
+import time
 from nav_msgs.msg import Odometry
 from task_generator.tasks import get_predefined_task
 from std_msgs.msg import Int16
@@ -9,60 +10,59 @@ from std_srvs.srv import Empty
 # for clearing costmap
 from clear_costmap import clear_costmaps
 
+
 class TaskGenerator:
     def __init__(self):
-        
+
         self.sr = rospy.Publisher('/scenario_reset', Int16, queue_size=1)
         self.nr = 0
         mode = rospy.get_param("~task_mode")
 
         scenarios_json_path = rospy.get_param("~scenarios_json_path")
         paths = {"scenario": scenarios_json_path}
-        self.task = get_predefined_task("",mode, PATHS=paths)
+        self.task = get_predefined_task("", mode, PATHS=paths)
 
         # if auto_reset is set to true, the task generator will automatically reset the task
         # this can be activated only when the mode set to 'ScenarioTask'
         auto_reset = rospy.get_param("~auto_reset")
-        self.start_time_= time.time()           
-        
+        self.start_time_ = time.time()
+
         # if the distance between the robot and goal_pos is smaller than this value, task will be reset
         #self.timeout_= rospy.get_param("~timeout")
-        self.timeout_= rospy.get_param("~timeout", 2.0)
-        self.timeout_= self.timeout_*60             # sec
+        self.timeout_ = rospy.get_param("~timeout", 2.0)
+        self.timeout_ = self.timeout_*60             # sec
         self.start_time_ = time.time()                # sec
         self.delta_ = rospy.get_param("~delta")
         robot_odom_topic_name = rospy.get_param(
             "robot_odom_topic_name", "odom")
-        
+
         auto_reset = auto_reset and (mode == "scenario" or mode == "random")
         self.curr_goal_pos_ = None
-        
-        
+
         if auto_reset:
             rospy.loginfo(
                 "Task Generator is set to auto_reset mode, Task will be automatically reset as the robot approaching the goal_pos")
             self.reset_task()
             self.robot_pos_sub_ = rospy.Subscriber(
-               robot_odom_topic_name, Odometry, self.check_robot_pos_callback)
+                robot_odom_topic_name, Odometry, self.check_robot_pos_callback)
 
-            rospy.Timer(rospy.Duration(0.5),self.goal_reached)
-            
+            rospy.Timer(rospy.Duration(0.5), self.goal_reached)
+
         else:
             # declare new service task_generator, request are handled in callback task generate
             self.reset_task()
             self.task_generator_srv_ = rospy.Service(
                 'task_generator', Empty, self.reset_srv_callback)
-                
+
         self.err_g = 100
-        
 
-    def goal_reached(self,event):
+    def goal_reached(self, event):
 
-        if self.err_g < 20*self.delta_:
+        if self.err_g < self.delta_:
             print(self.err_g)
             print("reached goal")
             self.reset_task()
-        if(time.time()-self.start_time_>self.timeout_):
+        if(time.time()-self.start_time_ > self.timeout_):
             print("timeout")
             self.reset_task()
 
@@ -75,13 +75,12 @@ class TaskGenerator:
     def reset_srv_callback(self, req):
         rospy.loginfo("Task Generator received task-reset request!")
         self.task.reset()
-        #return EmptyResponse()
-
+        # return EmptyResponse()
 
     def reset_task(self):
-        self.start_time_=time.time()
+        self.start_time_ = time.time()
         info = self.task.reset()
-        
+
         # clear_costmaps()
         # set goal position
         if info is not None:
@@ -91,7 +90,6 @@ class TaskGenerator:
         rospy.loginfo("".join(["="]*80))
         self.sr.publish(self.nr)
         self.nr += 1
-        
 
     def check_robot_pos_callback(self, odom_msg):
         # type: (Odometry) -> Any
@@ -102,7 +100,7 @@ class TaskGenerator:
         goal_y = self.curr_goal_pos_[1]
 
         self.err_g = (robot_x-goal_x)**2+(robot_y-goal_y)**2
-           
+
 
 if __name__ == '__main__':
     rospy.init_node('task_generator')
