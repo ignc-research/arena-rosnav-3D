7ca7824df53ac984e77b342bd157ba1615da6f3b
Duc Pichel <duc.pichel@gmail.com>
2022-02-23 17:47:51 +0100

    copy get_plots.py

--- arena_navigation/arena_local_planer/evaluation/arena_evaluation/03_plotting/get_plots_3D.py
+++ arena_navigation/arena_local_planer/evaluation/arena_evaluation/03_plotting/get_plots_3D.py
@@ -13,11 +13,10 @@ import scipy.ndimage as ndimage
 import seaborn as sns
 import pandas as pd
 import argparse
-import sys
 
 def parsing():
     parser = argparse.ArgumentParser(description='Create quantitative and qualitative plots for user.') # create parser object
-    parser.add_argument('--config', type=str, action='store', help='config yaml', default='get_plots_config_real.yaml') # store userID
+    parser.add_argument('--config', type=str, action='store', help='config yaml', default='get_plots_config_3D.yaml') # store userID
     args = parser.parse_args()
     return args
 
@@ -169,7 +168,7 @@ class plotter():
                     # plot map image
                     img= image.imread("{0}/{1}".format(map_path,map_file))
                     img_rotated = ndimage.rotate(img, 90, reshape=True) # rotate by 90 degree to get rviz konvention
-                    ax.imshow(img_rotated, cmap="gray")
+                    plt.imshow(img_rotated, cmap="gray")
 
                     # plot each planners path and if flag given collisions and zones
                     for key in sorted(obs_keys):
@@ -180,16 +179,16 @@ class plotter():
                         paths = self.data[key]["paths_travelled"]
                         episodes = paths.keys()
                         for i,episode in enumerate(episodes):
-                            if "3d" in planner:
-                                if "teb" in planner:
-                                    if i != 3:
-                                        continue
-                                else:
-                                    if i != 2:
-                                        continue
-                            else:
-                                if i != 0:
-                                    continue
+                            # if "3d" in planner:
+                            #     if "teb" in planner:
+                            #         if i != 3:
+                            #             continue
+                            #     else:
+                            #         if i != 2:
+                            #             continue
+                            # else:
+                            #     if i != 0:
+                            #         continue
                             if i == 0:
                                 if "3d" in planner:
                                     plt.plot([],[], # plot legend only with empty lines
@@ -298,10 +297,8 @@ class plotter():
                         ax.set_yticks(y_locs)
                         ax.set_yticklabels([])
 
-                    ax.xaxis.tick_top()
-
-                    plt.xlim(right=12/map_resolution)
-                    plt.ylim(bottom=20/map_resolution)
+                    # plt.xlim(right=12/map_resolution)
+                    # plt.ylim(bottom=20/map_resolution)
 
                     plt.savefig(self.plot_dir + "/qualitative_plots/qualitative_plot_{0}_{1}_{2}".format(map,obstacle_number,velocity), bbox_inches='tight',dpi=200)
                     fig.clear(True)
@@ -448,17 +445,15 @@ class plotter():
                             else:
                                 plt.title("Map: {0} Obstacles: {1} Velocity: {1}.{2} ".format(map, int(obstacle_number.replace("obs","")), velocity.replace("vel","")[0], velocity.replace("vel","")[1]), fontsize = self.config["plot_quantitative_title_size"])
                         
-                        # ax.get_xaxis().set_visible(False)
-                        for i,thisbar in enumerate(ax.patches):
-                            if i % 2 == 0:
-                                thisbar.set_hatch("/")
-                                thisbar.set_alpha(0.6)
+                        ax.get_xaxis().set_visible(False)
+                        # for i,thisbar in enumerate(ax.patches):
+                        #     if i % 2 == 0:
+                        #         thisbar.set_hatch("/")
+                        #         thisbar.set_alpha(0.6)
                         for label in ax.get_yaxis().get_ticklabels()[1::2]:
                             label.set_color('white')
-                        # for label in ax.get_yaxis().get_ticklabels()[::2]:
-                            # label.set_size(40)
-                        for label in ax.get_xaxis().get_ticklabels():
-                            label.set_size(10)
+                        for label in ax.get_yaxis().get_ticklabels()[::2]:
+                            label.set_size(40)
 
                         plt.savefig(self.plot_dir + "/quantitative_plots/{0}_{1}_{2}_{3}".format(metric,map,obstacle_number,velocity), bbox_inches='tight')
                         fig.clear(True)
@@ -558,15 +553,15 @@ class plotter():
                         else:
                             plt.title("Map: {0}".format(map), fontsize = self.config["plot_quantitative_title_size"])
 
-                    # ax.get_xaxis().set_visible(False)
-                    for i,thisbar in enumerate(ax.patches):
-                        if (i % 4) in [0,1]:
-                            thisbar.set_hatch("/")
-                            thisbar.set_alpha(0.6)
+                    ax.get_xaxis().set_visible(False)
+                    # for i,thisbar in enumerate(ax.patches):
+                    #     if (i % 4) in [0,1]:
+                    #         thisbar.set_hatch("/")
+                    #         thisbar.set_alpha(0.6)
                     for label in ax.get_yaxis().get_ticklabels()[1::2]:
                         label.set_color('white')
-                    # for label in ax.get_yaxis().get_ticklabels()[::2]:
-                    #     label.set_size(40)
+                    for label in ax.get_yaxis().get_ticklabels()[::2]:
+                        label.set_size(40)
 
                     plt.savefig(self.plot_dir + "/obs_in_one_plots/{0}_{1}_obs_in_one_{2}".format(metric,map,velocity), bbox_inches='tight')
                     fig.clear(True)
