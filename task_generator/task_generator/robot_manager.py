e429e5ab2df35f17c92ff9305b78ac211668ac2e
Jacenty00 <jacekxp60@gmail.com>
2022-05-09 15:52:44 +0200

    Update simple goal planners

--- task_generator/task_generator/robot_manager.py
+++ task_generator/task_generator/robot_manager.py
@@ -36,9 +36,7 @@ class RobotManager:
         self._srv_spawn_model = rospy.ServiceProxy(
             "/gazebo/spawn_urdf_model", SpawnModel
         )
-        self._goal_pub = rospy.Publisher(
-            "/goal", PoseStamped, queue_size=1, latch=True
-        )
+        self._goal_pub = rospy.Publisher("/goal", PoseStamped, queue_size=1, latch=True)
         self.pub_mvb_goal = rospy.Publisher(
             "/move_base_simple/goal", PoseStamped, queue_size=1, latch=True
         )
@@ -72,17 +70,13 @@ class RobotManager:
         start_pos.pose.position.z = 0.05
         rospy.wait_for_service("/gazebo/set_model_state")
         try:
-            set_state = rospy.ServiceProxy(
-                "/gazebo/set_model_state", SetModelState
-            )
+            set_state = rospy.ServiceProxy("/gazebo/set_model_state", SetModelState)
             set_state(start_pos)
 
         except rospy.ServiceException:
             print("Move Robot to position failed")
 
-        pub = rospy.Publisher(
-            "/initialpose", PoseWithCovarianceStamped, queue_size=10
-        )
+        pub = rospy.Publisher("/initialpose", PoseWithCovarianceStamped, queue_size=10)
 
         start_pos = PoseWithCovarianceStamped()
         start_pos.header.frame_id = "map"
@@ -105,14 +99,14 @@ class RobotManager:
         goal.pose = pose
 
         # wait for aio global planner
-        if self.planer == 'aio':
+        if self.planer == "aio":
             rospy.wait_for_service("/global_planner/makeGlobalPlan")
         else:
             # Make sure move_base is ready to take goals/make plan
             rospy.wait_for_service("/move_base/make_plan")
 
         # these planer need the nav-goal pulished to the /move_base_simple/goal topic
-        if self.planer in ["teb", "dwa", "mpc"]:
+        if self.planer in ["teb", "dwa", "mpc", "e2e"]:
             self.pub_mvb_goal.publish(goal)
         else:
             self._goal_pub.publish(goal)
